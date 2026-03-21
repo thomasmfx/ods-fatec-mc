ce25b4e027f398da72cea99e5d18c7e806211f3d
thomasmfx <thomasmoisesf@gmail.com>
2026-03-21 14:39:59 -0300

    chore: setup inicial do backend
    Instala e configura:
    
    - Java 21
    - Spring Boot 4
    - JdbcTemplate
    - springdoc-openapi
    - JJWT
    - Bucket4j
    
    Também remove banco do docker compose e deixa apenas swagger com o
    contrato da API

---
+++ backend/src/main/java/br/com/fatec/ods/OdsApplication.java
@@ -0,0 +1,15 @@
+package br.com.fatec.ods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdsApplication.class, args);
+
+        System.out.println("less gooo");
+	}
+
+}
---
+++ backend/src/main/java/br/com/fatec/ods/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package br.com.fatec.ods.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigins)
+                        .allowedMethods("GET", "POST")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
