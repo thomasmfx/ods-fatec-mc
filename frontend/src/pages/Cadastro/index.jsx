aede001b06f503da9c1949bd9f63fa1b74079a36
Talles Diniz <tallessdiniz@gmail.com>
2026-03-22 02:39:32 -0300

    fix: correção da formatação do input telefone

--- frontend/src/pages/Cadastro/index.jsx
+++ frontend/src/pages/Cadastro/index.jsx
@@ -80,7 +80,7 @@ function maskPhone(value) {
     return value
         .replaceAll(/\D/g, '')
         .replace(/^(\d{2})(\d)/, '($1) $2') 
-        .replace(/(\d{5})(\d{1,4})$/, '$1-$2')
+        .replace(/(\d{5})(\d{1,4})/, '$1-$2')
         .slice(0, 15)
 }
 
